a53997ae9a3cb86dd7753c982e540fb69f5908ea
Lêvy Alves <levy.pereiraA1532@gmail.com>
2024-11-13 23:08:39 -0300

    Criando controllers e views para os mesmos

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\IdeaController;
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::get('/', [HomeController::class, 'index'])->name('index');
+
+Route::resource('/categorias', CategoryController::class);
+Route::resource('/ideias', IdeaController::class);
+
+Route::get('/notas/{id}', [NoteController::class, 'show'])->name('notas.show');
