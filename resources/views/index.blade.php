b29ff4893343c01294c2ff6dd00c31fd7a8184ff
Lêvy Alves <levy.pereiraA1532@gmail.com>
2024-11-15 16:41:51 -0300

    Criando template para home e criando a home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,24 @@
-Dashboard
+@extends('_layout.base')
+
+@section('body')
+    <div class="container py-3">
+        <h2 class="mt-3">Seja Bem-Vindo ao Ideta 1.0</h2>
+        <hr>
+
+        <div class="d-flex">
+            <h4 class="mt-3 me-auto">Todas as Ideias</h4>
+
+            <form class="row g-3 align-items-center float-right">
+                <div class="col-auto">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected disabled>Selecione a categoria</option>
+                    </select>
+                </div>
+                <div class="col-auto">
+                    <button type="button" class="btn btn-success">Filtrar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
+
