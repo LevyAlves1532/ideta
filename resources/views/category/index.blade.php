b92e3dae8b35a58b9bae6effc67a44b307acc387
Lêvy Alves <levy.pereiraA1532@gmail.com>
2024-11-15 18:11:09 -0300

    Criando tabela na página de categorias e listando categorias

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -11,6 +11,51 @@
             <a href="#" class="btn btn-success float-right">Criar Categoria</a>
         </div>
         <hr>
-        Categorias
+        @if ($categories->count() > 0)
+            <table class="table table-hover table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Cor</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($categories as $category)
+                        <tr>
+                            <td style="vertical-align: middle;">{{ $category->name }}</td>
+                            <td style="vertical-align: middle;">
+                                <div class="d-flex" style="align-items: center; gap: 10px; text-transform: uppercase;">
+                                    <div style="width: 20px; height: 20px; border-radius: 5px; background-color: {{$category->color}};"></div>
+                                    {{ $category->color }}
+                                </div>
+                            </td>
+                            <td>
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Ações
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item text-secondary" href="{{ route('categorias.show', ['categoria' => $category->id]) }}">Visualizar</a></li>
+                                        <li><a class="dropdown-item text-primary" href="{{ route('categorias.edit', ['categoria' => $category->id]) }}">Editar</a></li>
+                                        <li>
+                                            <form id="form-{{ $category->id }}" method="POST" action="{{ route('categorias.destroy', ['categoria' => $category->id]) }}">
+                                                @csrf
+                                                @method('DELETE')
+                                                <a class="dropdown-item text-danger" href="#" onclick="document.getElementById('form-{{ $category->id }}').submit()">Deletar</a>
+                                            </form>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+            {{ $categories->links() }}
+        @else
+
+        @endif
     </div>
 @endsection
