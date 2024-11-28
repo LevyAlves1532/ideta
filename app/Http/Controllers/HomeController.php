b65764ad911f4028abee82fcb7117c18c1d7eae8
Lêvy Alves <levy.pereiraA1532@gmail.com>
2024-11-28 16:05:12 -0300

    Trazendo ultimas ideias trabalhadas

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,17 +3,48 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Idea;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $categories = Category::where('user_id', Auth::user()->id)->get();
+        $categoryId = $request->get('category_id') ?? '';
+
+        $latestIdeas = Idea::query()
+            ->where('user_id', Auth::user()->id)
+            ->when($categoryId, function ($query, $categoryId) {
+                if (!empty($categoryId)) {
+                    $query->whereHas('categories', function ($query) use ($categoryId) {
+                        $query->where('categories.id', $categoryId);
+                    });
+                }
+            })
+            ->where(function($query) {
+                return $query->whereNotNull('updated_at')
+                    ->orWhereHas('notes', function ($subQuery) {
+                        return $subQuery->whereNotNull('updated_at');
+                    });
+            })
+            ->orderBy(DB::raw('GREATEST(ideas.updated_at, (SELECT MAX(notes.updated_at) FROM notes WHERE notes.idea_id = ideas.id))'), 'desc')
+            ->take(5)
+            ->get();
+
+        $categoriesLatestIdeas = Category::query()
+            ->whereIn('id', function ($query) use ($latestIdeas) {
+                $query->select('category_id')
+                      ->from('categories_ideas');
+            })
+            ->distinct()
+            ->get();
 
         return view('index', [
-            'categories' => $categories,
+            'latestIdeas' => $latestIdeas,
+            'categoriesLatestIdeas' => $categoriesLatestIdeas,
+            'category_id' => $categoryId,
         ]);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,20 +12,67 @@
         <div class="d-flex">
             <h4 class="mt-3 me-auto">Ultimas Ideias Trabalhadas</h4>
 
-            <form class="row g-3 align-items-center float-right">
-                <div class="col-auto">
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected disabled>Selecione a categoria</option>
-                        @foreach ($categories as $category)
-                            <option value="{{ $category->slug }}">{{ $category->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="col-auto">
-                    <button type="button" class="btn btn-success">Filtrar</button>
-                </div>
-            </form>
+            @if ($latestIdeas->count() > 0)
+                <form class="row g-3 align-items-center float-right">
+                    <div class="col-auto">
+                        <select class="form-select" name="category_id">
+                            <option selected disabled>Selecione a categoria</option>
+                            @foreach ($categoriesLatestIdeas as $category)
+                                <option value="{{ $category->id }}" @if ($category->id == $category_id) selected @endif>{{ $category->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="col-auto">
+                        <button class="btn btn-success">Filtrar</button>
+                    </div>
+                    @if (!empty($category_id))
+                        <div class="col-auto">
+                            <a href="{{ route('index') }}" class="btn btn-primary">Limpar Filtro</a>
+                        </div>
+                    @endif
+                </form>
+            @endif
         </div>
+
+        @if ($latestIdeas->count() > 0)
+            <table class="table table-hover table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">Título</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($latestIdeas as $idea)
+                        <tr>
+                            <td style="vertical-align: middle;">{{ $idea->title }}</td>
+                            <td>
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Ações
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item text-secondary" href="{{ route('ideas.show', ['ideia' => $idea->id]) }}">Visualizar</a></li>
+                                        <li><a class="dropdown-item text-primary" href="{{ route('ideas.edit', ['ideia' => $idea->id]) }}">Editar</a></li>
+                                        <li>
+                                            <form id="form-{{ $idea->id }}" method="POST" action="{{ route('ideas.destroy', ['ideia' => $idea->id]) }}">
+                                                @csrf
+                                                @method('DELETE')
+                                                <a class="dropdown-item text-danger" href="#" onclick="document.getElementById('form-{{ $idea->id }}').submit()">Deletar</a>
+                                            </form>
+                                        </li>
+                                        <li><hr class="dropdown-divider"></li>
+                                        <li><a class="dropdown-item" href="{{ route('notes.index', ['idea_id' => $idea->id]) }}">Ver Anotações</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @else
+            <p class="text-center text-secondary">Não há ideias</p>
+        @endif
     </div>
 @endsection
 
