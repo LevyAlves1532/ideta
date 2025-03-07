6a76ea3ea764b36bb3fdd536fd0c4675a5527bbd
Lêvy Alves <levy.pereiraA1532@gmail.com>
2025-03-07 13:03:44 -0300

    Componentizando tabela da página home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,56 +8,58 @@
 @section('content')    
     <div class="row">
         <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">
+            @component('components.common.card')
+                @slot('card_header')
                     <p class="mb-0">Ultimas Ideias Trabalhadas</p>
-                </div>
+                @endslot
 
-                <div class="card-body p-0">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th style="width: 10px">#</th>
-                                <th>Ideia</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($latestIdeas as $latestIdea)
-                                <tr>
-                                    <td>{{ $latestIdea->id }}</td>
-                                    <td>{{ $latestIdea->title }}</td>
-                                    <td>
-                                        <div class="btn-group">
-                                            <div class="btn btn-success">Ações</div>
-                                            <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                                                <span class="sr-only">Toggle Dropdown</span>
-                                            </button>
-                                            <div class="dropdown-menu" role="menu" style="">
-                                                <a class="dropdown-item text-info" href="{{ route('ideas.show', ['ideia' => $latestIdea->id]) }}">Visualizar</a>
-                                                <a class="dropdown-item text-primary" href="{{ route('ideas.edit', ['ideia' => $latestIdea->id]) }}">Editar</a>
-                                                <form id="form-{{ $latestIdea->id }}" method="POST" action="{{ route('ideas.destroy', ['ideia' => $latestIdea->id]) }}">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button class="dropdown-item text-danger" onclick="document.getElementById('form-{{ $latestIdea->id }}').submit()">Deletar</button>
-                                                </form>
-                                                <div class="dropdown-divider"></div>
-                                                <a class="dropdown-item text-info" href="{{ route('notes.index', ['idea_id' => $latestIdea->id]) }}">Ver Anotações</a>
-                                                <form id="form-share-{{ $latestIdea->id }}" method="POST" action="{{ route('ideas.share-idea') }}">
-                                                    @csrf
-                                                    <input type="hidden" name="idea_id" value="{{ $latestIdea->id }}">
-                                                    <button class="dropdown-item text-success" onclick="document.getElementById('form-share-{{ $latestIdea->id }}').submit()">Compartilhar</button>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-
-                <div class="card-footer">
+                @component('components.common.table', [
+                    'columns' => [
+                        [
+                            'label' => '#',
+                            'style' => 'width: 10px',
+                        ],
+                        [
+                            'label' => 'Ideia:'
+                        ],
+                        [
+                            'label' => ''
+                        ]
+                    ],
+                ])
+                    @foreach ($latestIdeas as $latestIdea)
+                        <tr>
+                            <td>{{ $latestIdea->id }}</td>
+                            <td>{{ $latestIdea->title }}</td>
+                            <td>
+                                <div class="btn-group">
+                                    <div class="btn btn-success">Ações</div>
+                                    <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                                        <span class="sr-only">Toggle Dropdown</span>
+                                    </button>
+                                    <div class="dropdown-menu" role="menu" style="">
+                                        <a class="dropdown-item text-info" href="{{ route('ideas.show', ['ideia' => $latestIdea->id]) }}">Visualizar</a>
+                                        <a class="dropdown-item text-primary" href="{{ route('ideas.edit', ['ideia' => $latestIdea->id]) }}">Editar</a>
+                                        <form id="form-{{ $latestIdea->id }}" method="POST" action="{{ route('ideas.destroy', ['ideia' => $latestIdea->id]) }}">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="dropdown-item text-danger" onclick="document.getElementById('form-{{ $latestIdea->id }}').submit()">Deletar</button>
+                                        </form>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item text-info" href="{{ route('notes.index', ['idea_id' => $latestIdea->id]) }}">Ver Anotações</a>
+                                        <form id="form-share-{{ $latestIdea->id }}" method="POST" action="{{ route('ideas.share-idea') }}">
+                                            @csrf
+                                            <input type="hidden" name="idea_id" value="{{ $latestIdea->id }}">
+                                            <button class="dropdown-item text-success" onclick="document.getElementById('form-share-{{ $latestIdea->id }}').submit()">Compartilhar</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                @endcomponent
+                
+                @slot('card_footer')
                     <div class="form-group">
                         <label style="" for="categories">Categoria:</label>
                         <select class="form-control" id="categories" tabindex="-1" aria-hidden="true">
@@ -71,8 +73,8 @@
                     @if (!empty($category_id))
                         <a href="{{ route('index') }}" class="btn btn-primary">Limpar Filtro</a>
                     @endif
-                </div>
-            </div>
+                @endslot
+            @endcomponent
         </div>
     </div>
 @endsection
